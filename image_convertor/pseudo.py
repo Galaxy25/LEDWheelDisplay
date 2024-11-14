a6abdce41b3592c77cd4a7876957f13c6572fa7b
Nathan St. John <84703098+Galaxy25@users.noreply.github.com>
2024-11-14 10:16:48 -0800

    Create pseudo.py

---
+++ image_convertor/pseudo.py
@@ -0,0 +1,22 @@
+import math
+
+class Light_Controller(): 
+    def __init__(self, light_array):
+        self.light_array = light_array
+        self.index = 0
+    
+
+    def check_light(self, wheelPosition):
+        if wheelPosition <= self.light_array[index][0]:
+            self.index += 1
+        
+    def set_pixel():
+        pass
+
+    def reset_index():
+        index = 0
+
+
+exampleArray = ((0*math.PI, (255,0,0)), (0.5*math.PI, (0,255,0)), (0.5*math.PI, (0,255,0)), (1*math.PI, (255,0,255)))
+
+
